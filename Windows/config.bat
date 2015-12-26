8193db39f406ccab0e381ff5257f29978ec82b33
MattGuerrette <direct3Dtutorials@gmail.com>
2015-12-26 18:07:05 -0500

    updated windows build system to include HatchitGame, HatchitResource and all dependencies needed

--- Windows/config.bat
+++ Windows/config.bat
@@ -3,5 +3,5 @@ mkdir VS2013
 mkdir VS2015
 cd VS2013
 cmake ../../ -G "Visual Studio 12 2013 Win64"
-#cd ../VS2015
-#cmake ../../ -G "Visual Studio 14 2015 Win64"
+cd ../VS2015
+cmake ../../ -G "Visual Studio 14 2015 Win64"
