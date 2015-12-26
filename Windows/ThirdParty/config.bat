7b09d31c9b8dc50edb4906f5bd066d96c591a8ca
MattGuerrette <direct3Dtutorials@gmail.com>
2015-12-26 16:07:41 -0500

    added SDL2 to third party build system

--- Windows/ThirdParty/config.bat
+++ Windows/ThirdParty/config.bat
@@ -6,7 +6,7 @@ cd VS2013
 mkdir ThirdParty
 cd ThirdParty
 cmake ../../../ThirdParty/ -G "Visual Studio 12 2013 Win64"
-cd ../VS2015
+cd ../../VS2015
 mkdir ThirdParty
 cd ThirdParty
 cmake ../../../ThirdParty/ -G "Visual Studio 14 2015 Win64"
