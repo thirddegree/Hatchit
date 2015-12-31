83ff333f2d25c4369eb580c53de69213ad6216e1
MattGuerrette <direct3Dtutorials@gmail.com>
2015-12-31 17:31:35 -0500

    updated build system on windows.

--- Windows/ThirdParty/config.bat
+++ Windows/ThirdParty/config.bat
@@ -10,3 +10,28 @@ cd ../../VS2015
 mkdir ThirdParty
 cd ThirdParty
 cmake ../../../ThirdParty/ -G "Visual Studio 14 2015 Win64"
+cd ../../../
+
+REM Build Python libraries
+
+mkdir _build\dependencies\Python\bin\Debug
+mkdir _build\dependencies\Python\bin\Release
+mkdir _build\dependencies\Python\lib\Debug
+mkdir _build\dependencies\Python\lib\Release
+
+cd ../ThirdParty/Python/PCbuild
+call "build.bat" -p x64 -d
+
+move /Y x64-pgo\python3.lib %~dp0\..\_build\dependencies\Python\lib\Debug\
+move /Y amd64\python34_d.lib %~dp0\..\_build\dependencies\Python\lib\Debug\
+move /Y x64-pgo\python34stub.lib %~dp0\..\_build\dependencies\Python\lib\Debug\
+move /Y x64-pgo\* %~dp0\..\_build\dependencies\Python\bin\Debug\
+move /Y amd64\python34_d.dll %~dp0\..\_build\dependencies\Python\bin\Debug\
+
+call "build.bat" -p x64
+
+move /Y amd64\python3.lib %~dp0\..\_build\dependencies\Python\lib\Release\
+move /Y amd64\python34.lib %~dp0\..\_build\dependencies\Python\lib\Release\
+move /Y amd64\python34stub.lib %~dp0\..\_build\dependencies\Python\lib\Release\
+move /Y amd64\python3.dll %~dp0\..\_build\dependencies\Python\bin\Release\
+move /Y amd64\python34.dll %~dp0\..\_build\dependencies\Python\bin\Release\
--- Windows/config.bat
+++ Windows/config.bat
@@ -1,7 +1,28 @@
 cd _build
+mkdir bin\Debug
+mkdir bin\Release
+
+REM Place all third party libs into the Debug and Release bin folders
+
+copy /Y dependencies\SDL2\bin\Debug\SDL2.dll bin\Debug
+copy /Y dependencies\assimp\bin\Debug\assimp.dll bin\Debug
+copy /Y dependencies\glew\bin\Debug\glew32.dll bin\Debug
+copy /Y dependencies\tinyxml2\bin\Debug\tinyxml2.dll bin\Debug
+copy /Y dependencies\Python\bin\Debug\python3.dll bin\Debug
+copy /Y dependencies\Python\bin\Debug\python34_d.dll bin\Debug
+
+copy /Y dependencies\SDL2\bin\Release\SDL2.dll bin\Release
+copy /Y dependencies\assimp\bin\Release\assimp.dll bin\Release
+copy /Y dependencies\glew\bin\Release\glew32.dll bin\Release
+copy /Y dependencies\tinyxml2\bin\Release\tinyxml2.dll bin\Release
+copy /Y dependencies\Python\bin\Release\python3.dll bin\Release
+copy /Y dependencies\Python\bin\Release\python34.dll bin\Release
+
 mkdir VS2013
 mkdir VS2015
 cd VS2013
 cmake ../../ -G "Visual Studio 12 2013 Win64"
 cd ../VS2015
 cmake ../../ -G "Visual Studio 14 2015 Win64"
+
+
