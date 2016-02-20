01d3011272e126a788c5ee0fddda288a3e399e11
Honeybunch <amt3824@g.rit.edu>
2016-02-20 18:25:46 -0500

    Slight build system and asset tweaks

---
+++ Assets/Shaders/tri_FS.glsl
@@ -0,0 +1,7 @@
+#version 400
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_shading_language_420pack : enable
+layout (location = 0) out vec4 uFragColor;
+void main() {
+   uFragColor = vec4(1,0,0,1);
+}
