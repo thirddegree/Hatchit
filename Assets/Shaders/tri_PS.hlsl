d52d8b89068b44a8f8f8cae2f7079ea2bfc943b0
Jarrett Way <cjw6117@rit.edu>
2016-03-07 18:21:48 -0500

    updated shaders for dx.

--- Assets/Shaders/tri_PS.hlsl
+++ Assets/Shaders/tri_PS.hlsl
@@ -1,10 +1,14 @@
-struct PSInput
+// Per-pixel color data passed through the pixel shader.
+struct PixelShaderInput
 {
-    float4 position : SV_POSITION;
-    float4 color : COLOR;
+    float3 worldpos : POSITION;
+    float4 pos : SV_POSITION;
+    float3 color : COLOR0;
 };
 
-float4 main(PSInput input) : SV_TARGET
+// A pass-through function for the (interpolated) color data.
+float4 main(PixelShaderInput input) : SV_TARGET
 {
-	return input.color;
+
+    return float4(input.color, 1.0f);
 }
--- Assets/Shaders/tri_VS.hlsl
+++ Assets/Shaders/tri_VS.hlsl
@@ -1,22 +1,41 @@
-struct VSInput
+// A constant buffer that stores the three basic column-major matrices for composing geometry.
+cbuffer ModelViewProjectionConstantBuffer : register(b0)
 {
-	float4 position : POSITION;
-	float4 color    : COLOR;
+    matrix model;
+    matrix view;
+    matrix projection;
 };
 
-struct PSInput
+// Per-vertex data used as input to the vertex shader.
+struct VertexShaderInput
 {
-    float4 position : SV_POSITION;
-    float4 color : COLOR;
+    float3 pos : POSITION;
+    float3 color : COLOR0;
 };
 
-PSInput main(VSInput input)
+// Per-pixel color data passed through the pixel shader.
+struct PixelShaderInput
 {
-	PSInput result;
+    float3 worldpos : POSITION;
+    float4 pos : SV_POSITION;
+    float3 color : COLOR0;
+};
 
-	result.position = input.position;
-	result.color = input.color;
+// Simple shader to do vertex processing on the GPU.
+PixelShaderInput main(VertexShaderInput input)
+{
+    PixelShaderInput output;
+    float4 pos = float4(input.pos, 1.0f);
 
-	return result;
+    // Transform the vertex position into projected space.
+    pos = mul(pos, model);
+    output.worldpos = pos;
+    pos = mul(pos, view);
+    pos = mul(pos, projection);
+    output.pos = pos;
+
+    // Pass the color through without modification.
+    output.color = input.color;
+
+    return output;
 }
-
