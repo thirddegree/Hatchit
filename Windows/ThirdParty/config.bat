2974520b7c25a6ad901a0de55f3b7866f4133d0b
JamesMarcil <JamesMarcil@users.noreply.github.com>
2016-04-20 14:52:16 -0400

    Updating Windows config.bat to assume Python install.
    We now no longer build Python from source, and instead assume that a user
    has Python installed.

--- Windows/ThirdParty/config.bat
+++ Windows/ThirdParty/config.bat
@@ -10,37 +10,24 @@ cmake ../../../ThirdParty/ -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIM
 msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
 msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
 
-cd ../../../
-
-REM Build Python libraries
-mkdir _build\dependencies\Python\bin\
-mkdir _build\dependencies\Python\DLLs\
-mkdir _build\dependencies\Python\Lib\
-mkdir _build\dependencies\Python\libs\
-
-cd ../ThirdParty/Python/PCbuild
-
-REM Python Debug build
-call "get_externals.bat"
-call "build.bat" -p x64 -d -e
-move /Y .\amd64\python_d.exe %~dp0..\_build\dependencies\Python\bin
-move /Y .\amd64\*.dll %~dp0..\_build\dependencies\Python\bin
-move /Y .\amd64\*.lib %~dp0..\_build\dependencies\Python\libs
-move /Y .\amd64\*.pyd %~dp0..\_build\dependencies\Python\DLLs
-xcopy /Y /D /E ..\Lib %~dp0..\_build\dependencies\Python\Lib
-
-cd ..\..\virtualenv
-
 REM Create virtualenv
-setlocal
-set PYTHONPATH=%~dp0..\_build\dependencies\Python\Lib:%~dp0..\_build\dependencies\Python\DLLs
-%~dp0..\_build\dependencies\Python\bin\python_d.exe .\virtualenv.py %~dp0..\_build\dependencies\Python\env
-endlocal
+set PYTHONENV=%~dp0..\_build\dependencies\Python\
+cd ..\..\..\..\ThirdParty\virtualenv
+call python.exe .\virtualenv.py %PYTHONENV%
+if NOT %ERRORLEVEL% == 0 goto :Error
 
 REM Install libraries
-cd %~dp0..\_build\dependencies\Python\env\Scripts\
+cd %PYTHONENV%Scripts\
 call "activate.bat"
 pip install -I cogapp==2.5
 call "deactivate.bat"
+goto :Done
 
-REM TODO: Python Release build
+:Error
+echo "Error invoking Python. Is Python3.X.X installed? Is Python3.X.X accessible on the PATH?"
+goto :EndOfScript
+
+:Done
+echo "Script complete."
+
+:EndOfScript
