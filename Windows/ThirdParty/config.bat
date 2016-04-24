87ecc08f82700597576103871e5eb3ef9b6f398b
MattGuerrette <direct3Dtutorials@gmail.com>
2016-04-24 12:33:16 -0400

    removed Python dependency. Now assume full python install. Also added installation for libsndfile to build.

--- Windows/ThirdParty/config.bat
+++ Windows/ThirdParty/config.bat
@@ -10,36 +10,12 @@ cmake ../../../ThirdParty/ -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIM
 msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
 msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
 
+cd ../../../ThirdParty
 
-cd ../../../
+REM Download libsndfile installer
 
-REM Build Python libraries
-set PYTHONPATH=%PYTHONPATH%;C:\Python34\Lib
+pip install wget
 
-mkdir _build\dependencies\Python\bin\Debug
-mkdir _build\dependencies\Python\bin\Debug\Lib
-mkdir _build\dependencies\Python\bin\Release
-mkdir _build\dependencies\Python\bin\Release\Lib
-mkdir _build\dependencies\Python\lib\Debug
-mkdir _build\dependencies\Python\lib\Release
+python.exe libsndfile_install.py
 
-cd ../ThirdParty/Python/PCbuild
-call "build.bat" -p x64 -d
 
-move /Y x64-pgo\python3.lib %~dp0\..\_build\dependencies\Python\lib\Debug\
-move /Y amd64\python35_d.lib %~dp0\..\_build\dependencies\Python\lib\Debug\
-move /Y x64-pgo\python35stub.lib %~dp0\..\_build\dependencies\Python\lib\Debug\
-move /Y x64-pgo\* %~dp0\..\_build\dependencies\Python\bin\Debug\
-move /Y amd64\python35_d.dll %~dp0\..\_build\dependencies\Python\bin\Debug\
-move /Y amd64\python_d.exe %~dp0\..\_build\dependencies\Python\bin\Debug\
-xcopy /Y ..\Lib %~dp0\..\_build\dependencies\Python\bin\Debug\Lib /D /E
-
-call "build.bat" -p x64
-
-move /Y amd64\python3.lib %~dp0\..\_build\dependencies\Python\lib\Release\
-move /Y amd64\python35.lib %~dp0\..\_build\dependencies\Python\lib\Release\
-move /Y amd64\python35stub.lib %~dp0\..\_build\dependencies\Python\lib\Release\
-move /Y amd64\python3.dll %~dp0\..\_build\dependencies\Python\bin\Release\
-move /Y amd64\python35.dll %~dp0\..\_build\dependencies\Python\bin\Release\
-move /Y amd64\python.exe %~dp0\..\_build\dependencies\Python\bin\Release\
-xcopy /Y ..\Lib %~dp0\..\_build\dependencies\Python\bin\Release\Lib /D /E
