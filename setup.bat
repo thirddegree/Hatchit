c6e13dfab874fa432c3dda17d6a1627843831dac
MattGuerrette <direct3Dtutorials@gmail.com>
2016-09-01 12:24:55 -0400

    updated bat file to remove warnings. Also setup initial Windows Hatchit build.

--- setup.bat
+++ setup.bat
@@ -1,8 +1,23 @@
-mkdir build
-cd build
-mkdir ThirdParty
-cd ThirdParty
+@echo off
 
-cmake ../../ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIMP_BUILD_ASSIMP_TOOLS=NO
-msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
-msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
+IF NOT EXIST  %~dp0build mkdir build
+
+cd build
+
+IF NOT EXIST %cd% mkdir ThirdParty
+
+IF "%1" == "-hatchit" (
+    cmake ../ -G "Visual Studio 14 2015 Win64"
+)
+
+IF NOT "%1" == "-hatchit" (
+    cd ThirdParty
+
+    cmake ../../ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIMP_BUILD_ASSIMP_TOOLS=NO
+    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
+    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
+
+    cd ..
+
+    cmake ../ -G "Visual Studio 14 2015 Win64"
+)
