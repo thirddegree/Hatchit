86a290f8f1bfe9c7b4e8296d92f4b53bf4df341e
MattGuerrette <direct3Dtutorials@gmail.com>
2017-03-04 13:13:45 -0500

    corrected small syntax issues with setup script. Added freetype to thirdparty build

--- setup.bat
+++ setup.bat
@@ -7,17 +7,17 @@ cd build
 IF NOT EXIST %~dp0ThirdParty mkdir ThirdParty
 
 IF "%1" == "-hatchit" (
-    cmake ../ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
+    cmake ..\\ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
 )
 
 IF NOT "%1" == "-hatchit" (
     cd ThirdParty
 
-    cmake ../../ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DBUILD_SHARED_LIBS=ON
+    cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DBUILD_SHARED_LIBS=ON
     msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
     msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
 
     cd ..
 
-    cmake ../ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
+    cmake ..\\ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
 )
