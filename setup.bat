1f4113ef39b9f82487dca351fec500df8d9f3830
MattGuerrette <direct3Dtutorials@gmail.com>
2017-05-03 00:07:43 -0400

    updated build to include glbinding submodule. Also fixed batch script

--- setup.bat
+++ setup.bat
@@ -1,23 +1,29 @@
 @echo off
 
-IF NOT EXIST  %~dp0build mkdir build
+IF NOT EXIST  build mkdir build
 
 cd build
 
-IF NOT EXIST %~dp0ThirdParty mkdir ThirdParty
+IF NOT EXIST ThirdParty mkdir ThirdParty
 
 IF "%1" == "-hatchit" (
     cmake ..\\ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
 )
 
 IF NOT "%1" == "-hatchit" (
+
     cd ThirdParty
 
-    cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DBUILD_SHARED_LIBS=ON
+    cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DBUILD_SHARED_LIBS=ON -DBUILD_REQUIRED=ON
     msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
     msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
-
+    
+    cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DBUILD_REQUIRED=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DASSIMP_BUILD_TESTS=NO  -DBUILD_SHARED_LIBS=ON
+    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
+    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
+    
     cd ..
 
     cmake ..\\ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
+    
 )
