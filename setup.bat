7caba4df44fa87ffb70c085a7b8d420cc5de8df4
MattGuerrette <direct3Dtutorials@gmail.com>
2017-05-07 09:47:09 -0400

    UWP Build Changes
    Started updating build to support WinRT

--- setup.bat
+++ setup.bat
@@ -14,16 +14,22 @@ IF NOT "%1" == "-hatchit" (
 
     cd ThirdParty
 
-    cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DDIRECTX=NO -DBUILD_SHARED_LIBS=ON -DBUILD_REQUIRED=ON
-    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
-    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
-    
-    cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DBUILD_REQUIRED=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DASSIMP_BUILD_TESTS=NO  -DBUILD_SHARED_LIBS=ON
-    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
-    msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
-    
-    cd ..
+    if "%1" == "-winrt" (
+        cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_NAME:STRING=WindowsStore -DCMAKE_SYSTEM_VERSION:STRING=10.0 -DBUILD_SHARED_LIBS=ON -DBUILD_WINRT=ON
+        msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
+        msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
 
-    cmake ..\\ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
+        cd ..
+
+        cmake ..\\ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_NAME:STRING=WindowsStore -DCMAKE_SYSTEM_VERSION:STRING=10.0 -DBUILD_SHARED_LIBS=ON -DBUILD_WINRT=ON
+    )
+
+    REM cmake ..\\..\\ThirdParty -G "Visual Studio 14 2015 Win64" -DBUILD_REQUIRED=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DASSIMP_BUILD_TESTS=NO  -DBUILD_SHARED_LIBS=ON
+    REM msbuild.exe ThirdPartyLibs.sln /p:Configuration=Debug
+    REM msbuild.exe ThirdPartyLibs.sln /p:Configuration=Release
+    
+    REM cd ..
+
+    REM cmake ..\\ -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0 -DBUILD_SHARED_LIBS=ON
     
 )
